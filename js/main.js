cfff89e23c46a58c308491666a007141b544eb51
Alexander Al-Sughari <89976396+AlexanderAlSughari@users.noreply.github.com>
2021-10-01 05:10:05 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,159 @@
+function OtworzOkno_AlSughari_z1()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie1.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z2()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie2.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z3()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie3.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z4()
+{
+    var height = 400;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/2)-(height/2);
+
+    window.open("pages/zadanie4.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z5()
+{
+    var height = 400;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/2)-(height/2);
+
+    window.open("pages/zadanie5.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z6()
+{
+    var height = 400;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/2)-(height/2);
+
+    window.open("pages/zadanie6.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z7()
+{
+    var height = 400;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/2)-(height/2);
+
+    window.open("pages/zadanie7.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z8()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie8.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z9()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie9.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z10()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie10.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z11()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie11.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z12()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie12.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z13()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie13.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z14()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie14.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z15()
+{
+    var height = 100;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/4)*3;
+
+    window.open("pages/zadanie15.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
+
+function OtworzOkno_AlSughari_z16()
+{
+    var height = 400;
+    var width = 800;
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/2)-(height/2);
+
+    window.open("pages/zadanie16.html", "", "height="+height+", width="+width+", top="+top+", left="+left+"");
+}
